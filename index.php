885a18b1f9e8131cbe1593d0762327cc00716355
PythonTester <geoline.denisse@gmail.com>
2025-03-16 16:04:35 +0100

    Commit calculateur IMC et Readme

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>IMC Calcul</title>
+</head>
+
+<body class="bg-primary text-white">
+
+    <!--FORMULAIRE -->
+    <div id="form-container" class="container text-center">
+        <h1 class="p-2 mt-3 mb-3">Calculez votre IMC</h1>
+
+        <form id="imc-form" action="index.php" method="POST">
+
+            <div class="input-group mb-3"> <!--Déclarer son âge (pas requis, n'entre pas dans le calcul-->
+                <span class="input-group-text" id="basic-addon1">âge</span>
+                <input id="age" type="number" name="age" min="1" max="120" class="form-control" placeholder="ans">
+            </div>
+
+            <div class="input-group mb-3"> <!--Déclarer son poids : requis-->
+                <span class="input-group-text" id="basic-addon1">poids</span>
+                <input id="weight" type="number" name="weight" min="1" max="200" step="0.01" required class="form-control" placeholder="kilogrammes">
+            </div>
+
+            <div class="input-group mb-3"> <!--Déclarer sa taille : requis-->
+                <span class="input-group-text" id="basic-addon1">taille</span>
+                <input id="height" type="number" name="height" min="1" max="3" step="0.01" required class="form-control" placeholder="mètres">
+            </div>
+
+            <input id="submit" type="submit" value="Submit" class="btn btn-light">
+        </form>
+        <br><br>
+
+        <?php
+        //RECUPERATION DES DONNEES DU FORMULAIRE
+        if (isset($_POST['age']) && isset($_POST['weight']) && isset($_POST['height'])):
+            $age = $_POST['age'];
+            $weight = $_POST['weight'];
+            $height = $_POST['height'];
+        ?>
+
+
+            <!--CONTROLE L'AFFICHAGE DE L'IMC ET DU MESSAGE CORRESPONDANT-->
+            <div id="imc-result" class="p-2 form-control">
+                <p>Votre IMC est de :
+                <div id="imc-display" class="mb-2 form-control text-center">
+                    <?php $imc = $weight / pow($height, 2); ?>
+                    <?php echo round($imc, 2); ?>
+                </div>
+
+                <?php
+
+                $phrase = "Vous êtes en état de ";
+
+                if ($imc > 35):
+                    $etat = "obésité sévère !";
+                ?>
+                    <div id="imc-category" class="form-control border-danger">
+                        <p class="text-danger-emphasis text-center"><?php echo $phrase . $etat ?></p>
+                    </div>
+                <?php
+                elseif ($imc > 30):
+                    $etat = "obésité modérée !";
+                ?>
+                    <div id="imc-category" class="form-control border-danger-subtle">
+                        <p class="text-danger text-center"><?php echo $phrase . $etat ?></p>
+                    </div>
+                <?php
+                elseif ($imc > 25):
+                    $etat = "surpoids !"
+                ?>
+                    <div id="imc-category" class="form-control border-warning">
+                        <p class="text-warning-emphasis text-center"><?php echo $phrase . $etat ?></p>
+                    </div>
+                <?php
+                elseif ($imc > 18.5):
+                    $etat = "corpulence normale !";
+                ?>
+                    <div id="imc-category" class="form-control border-info-subtle">
+                        <p class="text-primary text-center"><?php echo $phrase . $etat ?></p>
+                    </div>
+                <?php
+                elseif ($imc <= 18.5) :
+                    $etat = "insuffisance pondérale !";
+                ?>
+                    <div id="imc-category" class="form-control border-danger">
+                        <p class="text-danger-emphasis text-center"><?php echo $phrase . $etat ?></p>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+
+
+
+
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Calculateur d'IMC
+
+## Contexte
+
+Il s'agit de la première réalisation en langage **PHP** dans laquelle j'associais à la fois le visuel et la logique et cela avec un certain succès. Il s'agit d'une application simple, comme on peut en trouver de nombreuses sur internet. Je n'ai ici pas réinventé la roue, mais je suis fière du résultat final, et d'avoir su surmonter la difficulté de "mélanger" deux langages différents dans une même réalisation. C'était également le premier formulaire qui employait la méthode `POST`, et dont les données étaient utilisées.
+
+## Description
+
+Ce calculateur d'IMC contient trois champs à renseigner par l'utilisateur : l'âge, le poids, et la taille. En fonction du résultat, il annonce à l'utilisateur dans quelle catégorie il se situe. Le visuel varie également en fonction de la catégorie affichée.
+
+## Langages
+
+Visuel : **HTML**, **CSS**/**Bootstrap** (CDN)
+Logique : **PHP**
+
+## Organisation du projet
+
+Un fichier unique index.php, qui contient tous les éléments, dont le **CSS**.
+
+## À venir ...
+
+Ce caclulateur n'est pas la première application simple que je crée. Je commiterais à l'avenir un générateur de phrase 'pseudonyme' réalisée avec JavaScript. Il s'agissait de ma première utilisation concrète des notions d'algorithmie, avec des boucles sur des tableaux. Je vous remercie de l'attention que vous portez à mes réalisations !
